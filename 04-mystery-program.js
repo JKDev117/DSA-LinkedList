6a275f219b6d63c07b664ffca7faf0d575f5cf6c
Jae Kim <jckim81@yahoo.com>
2020-06-29 09:17:52 -0400

    completed q4

--- 04-mystery-program.js
+++ 04-mystery-program.js
@@ -1,21 +1,29 @@
 /* 4. Mystery program
 
-Analyze the following function (without running it in an IDE) to determine what problem it is trying to solve. What is the time complexity of this algorithm?
+Analyze the following function (without running it in an IDE) to determine what problem it is trying to solve. 
+
+What is the time complexity of this algorithm?
 
 function WhatDoesThisProgramDo(lst) {
-    let current = lst.head;
-    while (current !== null) {
-        let newNode = current;
-        while (newNode.next !== null) {
-            if (newNode.next.value === current.value) {
-                newNode.next = newNode.next.next;
+    let current = lst.head; //O(1)
+    while (current !== null) { //O(n)
+        let newNode = current; //O(1)
+        while (newNode.next !== null) { //O(n)
+            if (newNode.next.value === current.value) { //O(1)
+                newNode.next = newNode.next.next; //O(1)
             }
             else {
-                newNode = newNode.next;
+                newNode = newNode.next; //O(1)
             }
         }
-        current = current.next;
+        current = current.next; //O(1)
     }
 }
 
 */
+
+//A1. It is trying to solve the problem of there being duplicates in the list.
+
+
+//A2.  O(n^2) ?
+
